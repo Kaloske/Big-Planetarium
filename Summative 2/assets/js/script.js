f85f87a444229ec5566bd5bb673e1a2b8907db7a
Kaloske <69522073+Kaloske@users.noreply.github.com>
2026-05-03 11:32:37 +0400

    Uploader proper file structure, changed to relative references for assets, added drop down menu to navigation, modified layout to use CSS Grid, added transition when hovering over planets.

--- Summative 2/assets/js/script.js
+++ Summative 2/assets/js/script.js
@@ -1,11 +1,10 @@
 const img_swap = (button, new_img, img_element) => {
 
-    img = document.getElementById(img_element);
+    let img = document.getElementById(img_element);
     // all images are in assets/images, so we do this for brevity
-    new_img = "/assets/images/" + new_img;
-
 
     if (img.getAttribute('src') !== new_img) {
+
         img.src = new_img;
         for (const child of button.parentElement.children) {
             if (child !== button) {
@@ -15,5 +14,19 @@ const img_swap = (button, new_img, img_element) => {
         }
         button.classList.toggle("btn-highlight")
 
+    }
+};
+
+// Nav dropdown code
+let visible = false
+const dropdown = () => {
+    let content = document.getElementById("dropdown-content")
+
+    if (visible === false) {
+        content.style.display = "block"
+        visible = true
+    } else {
+        content.style.display = "none"
+        visible = false
     }
 };
